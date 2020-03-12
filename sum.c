4e789f4759e48abca44501846fc1e1e3affd5903
SaadDroid <60286759+SaadDroid@users.noreply.github.com>
2020-03-11 19:43:08 -0700

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	//this is a comment
+	int a,b=3, c=4;
+	a=b+c;
+	printf("%d",a);
+	return 0;
+}
---
+++ sum1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a,b=3, c=4;
+	a=b+c;
+	printf("%d",a);
+	return 0;
+//no comment
+}
